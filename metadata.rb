4f6cd0a5183ee72b8a130d67c765fb4b95bd889a
galstrom21 <jshepher@rackspace.com>
2014-03-27 14:39:09 -0500

    Adding Berksfile.lock for stable branch
            * removed Berksfile.lock from .gitignore
            * added defaultbranch to .gitreview
            * added branch tag to Berksfile dependencies
            * Removed extra deps from Berksfile and metadata.rb
    
    Change-Id: If9dba8503c360835aeb1df99d1bca964c4d4e2b5

--- metadata.rb
+++ metadata.rb
@@ -6,13 +6,13 @@ description      'The OpenStack Metering service Ceilometer.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '8.4.0'
 
-recipe 'openstack-telemetry::agent-central', 'Installs agent central service.'
-recipe 'openstack-telemetry::agent-compute', 'Installs agent compute service.'
-recipe 'openstack-telemetry::api', 'Installs API service.'
-recipe 'openstack-telemetry::client', 'Installs client.'
-recipe 'openstack-telemetry::collector', 'Installs collector service. If the NoSQL database is used for metering service, ceilometer-dbsync will not be executed.'
-recipe 'openstack-telemetry::common', 'Common metering configuration.'
-recipe 'openstack-telemetry::identity_registration', 'Registers the endpoints, tenant and user for metering service with Keystone'
+recipe           'openstack-telemetry::agent-central', 'Installs agent central service.'
+recipe           'openstack-telemetry::agent-compute', 'Installs agent compute service.'
+recipe           'openstack-telemetry::api', 'Installs API service.'
+recipe           'openstack-telemetry::client', 'Installs client.'
+recipe           'openstack-telemetry::collector', 'Installs collector service. If the NoSQL database is used for metering service, ceilometer-dbsync will not be executed.'
+recipe           'openstack-telemetry::common', 'Common metering configuration.'
+recipe           'openstack-telemetry::identity_registration', 'Registers the endpoints, tenant and user for metering service with Keystone'
 
 %w{ ubuntu suse }.each do |os|
   supports os
