ddbab534e00ed456845414116148d68dbea00e93
Jan Klare <j.klare@cloudbau.de>
2016-06-23 17:00:38 +0200

    initial commit for the newton development cycle
    Change-Id: I4594c66bba8bde96562d838b1fbc1f05d7961ab0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'openstack-dev@lists.openstack.org'
 license 'Apache 2.0'
 description 'The OpenStack Metering service Ceilometer.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '13.0.0'
+version '14.0.0'
 
 recipe 'openstack-telemetry::agent-central', 'Installs agent central service.'
 recipe 'openstack-telemetry::agent-compute', 'Installs agent compute service.'
@@ -21,5 +21,5 @@ recipe 'openstack-telemetry::identity_registration', 'Registers the endpoints, t
   supports os
 end
 
-depends 'openstack-common', '>= 13.0.0'
-depends 'openstack-identity', '>= 13.0.0'
+depends 'openstack-common', '>= 14.0.0'
+depends 'openstack-identity', '>= 14.0.0'
